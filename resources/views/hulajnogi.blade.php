7c896e5e874871722bcf884a6e4427643443af7d
s38980 <s38980.student@pwste.edu.pl>
2023-05-25 10:20:58 +0200

    hulajnogi-init

--- resources/views/hulajnogi.blade.php
+++ resources/views/hulajnogi.blade.php
@@ -2,29 +2,31 @@
 <head>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>HULAJNOGIl</title>
+    <title>HULAJNOGI</title>
 </head>
 <body>
-<h1>List of Hulajnogiij</h1>
-<table>
-    <thread>
+<h1>Lista Hulajnóg</h1>
+<table class="table table-striped table-bordered">
+    <thead>
         <tr>
-            <th>id</th>
-            <th>nazwa</th>
-            <th>model</th>
-
+            <th>Nazwa</th>
+            <th>Model</th>
         </tr>
-    </thread>
+    </thead>
+    <tbody>
+        @foreach ($hulajnogi as $hulajnoga)
+            <tr>
+                <td>{{ $hulajnoga->Nazwa }}</td>
+                <td>{{ $hulajnoga->Model }}</td>
+                <td>
+                    <button class="btn btn-primary btn-sm">Edycja</button>
+                    <button class="btn btn-danger btn-sm">Usuń</button>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
 </table>
 
-    @foreach ($hulajnogi as $hulajnoga)
-
-    <p>{{ $hulajnoga->Nazwa }}</p>
-    <p>{{ $hulajnoga->Model }}</p>
-
-
-
-@endforeach
 <?php
     use App\Models\Hulajnogi;
     /*$rekordy=Hulajnogi::factory()->count(5)->create();*/
