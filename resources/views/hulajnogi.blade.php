173de242e4dedfa8a3a04ac4d95eb6dcf3041199
MaciejRokitowski <rokitowskimaciej@gmail.com>
2023-05-25 09:37:27 +0200

    Strona hulajnogi

--- resources/views/hulajnogi.blade.php
+++ resources/views/hulajnogi.blade.php
@@ -1,15 +1,27 @@
 <html>
 <head>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>HULAJNOGI</title>
 </head>
 <body>
-<h1>Hulajnogi</h1>
-<?php
+<h1>List of Hulajnogi</h1>
+<table>
+    <thread>
+        <tr>
+            <th>id</th>
+            <th>nazwa</th>
+            <th>model</th>
+
+        </tr>
+    </thread>
+</table>
+
     @foreach ($hulajnogi as $hulajnoga)
-    {
-    {{ $hulajnoga->nazwa }}
-    }
+
+    <p>{{ $hulajnoga->nazwa }}</p>
+    <p>{{$hulajnoga->model}}</p>
+
 @endforeach
-?>
+
 </body>
 </html>
