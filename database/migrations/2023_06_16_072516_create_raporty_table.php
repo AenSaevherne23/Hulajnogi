45554793c28578e090a778253c692181997d72bc
s38980 <s38980.student@pwste.edu.pl>
2023-06-16 10:29:54 +0200

    raporty-dodano baze oraz model

--- database/migrations/2023_06_16_072516_create_raporty_table.php
+++ database/migrations/2023_06_16_072516_create_raporty_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('raporty', function (Blueprint $table) {
             $table->id();
+            $table->date('data');
+            $table->integer('liczba_wypozyczen');
+            $table->integer('liczba_odbiorow');
+            $table->integer('liczba_uszkodzonych');
+            $table->decimal('zysk');
+            $table->unsignedBigInteger('placowka_id')->nullable();
+            $table->unsignedBigInteger('odbiory_id')->nullable();
+            $table->unsignedBigInteger('rewizje_id')->nullable();
+            $table->unsignedBigInteger('wypozyczenia_id')->nullable();
+
+            $table->foreign('placowka_id')->references('id')->on('placowki')->onDelete('set null');
+            $table->foreign('odbiory_id')->references('id')->on('odbiory')->onDelete('set null');
+            $table->foreign('rewizje_id')->references('id')->on('rewizje')->onDelete('set null');
+            $table->foreign('wypozyczenia_id')->references('id')->on('wypozyczenia')->onDelete('set null');
+
             $table->timestamps();
         });
     }
